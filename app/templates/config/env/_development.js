18c8f7cf1359746b8296aa4873b1437614391dfb
wangshijun <wangshijun2010@gmail.com>
2015-04-12 10:07:44 +0800

    add dev config

---
+++ app/templates/config/env/_development.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+    db: 'mongodb://localhost/<%= slugifiedAppName %>',
+    port: 3000,
+    app: {
+        title: '<%= appName %> - Development Environment'
+    },
+    facebook: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/facebook/callback'
+    },
+    twitter: {
+        clientID: 'CONSUMER_KEY',
+        clientSecret: 'CONSUMER_SECRET',
+        callbackURL: 'http://localhost:3000/auth/twitter/callback'
+    },
+    google: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/google/callback'
+    },
+    linkedin: {
+        clientID: 'APP_ID',
+        clientSecret: 'APP_SECRET',
+        callbackURL: 'http://localhost:3000/auth/linkedin/callback'
+    }
+};
